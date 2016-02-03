504f2ce4dc14cc674645483b6b38222beecf84dc
PavelKizernis <pashakiz@gmail.com>
2016-02-03 17:18:56 +0300

    some edits

--- html/js/common.js
+++ html/js/common.js
@@ -27,60 +27,60 @@ $(document).ready(function() {
 	}
 
 	placeholder(".feedback_name");
+	placeholder(".feedback_org");
 	placeholder(".feedback_email");
-	placeholder(".feedback_message");
 
 	//Parallax (Stellar)
 	//Документация: http://markdalgleish.com/projects/stellar.js/docs/
 	//<div class="image" data-stellar-background-ratio="0">...</div>
-	$.stellar({
-		horizontalScrolling: false,
-		verticalOffset: 0
-	});
+	// $.stellar({
+	// 	horizontalScrolling: false,
+	// 	verticalOffset: 0
+	// });
 	
 	//Попап менеджер FancyBox
 	//Документация: http://fancyapps.com/fancybox/
 	//<a class="fancybox" rel="group" href="big_image_1.jpg"><img src="small_image_1.jpg" alt="" /></a>
 	//<a class="fancybox" rel="group" href="big_image_2.jpg"><img src="small_image_2.jpg" alt="" /></a>
-	$(".fancybox").fancybox();
+	// $(".fancybox").fancybox();
 
 	//Навигация по Landing Page
 	//$(".top_mnu") - это верхняя панель со ссылками.
 	//Ссылки вида <a href="#contacts">Контакты</a>
-	$(".top_mnu").navigation();
+	// $(".top_mnu").navigation();
 
 	//Добавляет классы дочерним блокам .block для анимации
 	//Документация: http://imakewebthings.com/jquery-waypoints/
-	$(".block").waypoint(function(direction) {
-		if (direction === "down") {
-			$(".class").addClass("active");
-		} else if (direction === "up") {
-			$(".class").removeClass("deactive");
-		};
-	}, {offset: 100});
+	// $(".block").waypoint(function(direction) {
+	// 	if (direction === "down") {
+	// 		$(".class").addClass("active");
+	// 	} else if (direction === "up") {
+	// 		$(".class").removeClass("deactive");
+	// 	};
+	// }, {offset: 100});
 
 	//Плавный скролл до блока .div по клику на .scroll
 	//Документация: https://github.com/flesler/jquery.scrollTo
-	$("a.scroll").click(function() {
-		$.scrollTo($(".div"), 800, {
-			offset: -90
-		});
-	});
+	// $("a.scroll").click(function() {
+	// 	$.scrollTo($(".div"), 800, {
+	// 		offset: -90
+	// 	});
+	// });
 
 	//Каруселька
 	//Документация: http://www.owlcarousel.owlgraphic.com/docs/started-welcome.html
-	$(".carousel").owlCarousel();
+	// $(".carousel").owlCarousel();
 
 	//Кнопка "Наверх"
 	//Документация:
 	//http://api.jquery.com/scrolltop/
 	//http://api.jquery.com/animate/
-	$("#top").click(function () {
-		$("body, html").animate({
-			scrollTop: 0
-		}, 800);
-		return false;
-	});
+	// $("#top").click(function () {
+	// 	$("body, html").animate({
+	// 		scrollTop: 0
+	// 	}, 800);
+	// 	return false;
+	// });
 	
 	//Аякс отправка форм
 	//Документация: http://api.jquery.com/jquery.ajax/
@@ -90,7 +90,7 @@ $(document).ready(function() {
 			url: "mail.php",
 			data: $("form").serialize(),
 			success: function(data) {
-				//$('#order_status').html(data);
+				// $('#order_status').html(data);
 				$('#order_status').html('Спасибо, Ваша заявка отправлена!');
 			},
 			error:  function(xhr, str){
@@ -106,8 +106,8 @@ $(document).ready(function() {
 	});
 
 	//show
-	$(".feedback_btn-turn").on("click", function() {
-		$("#feedback").show();
-	});
+	// $(".feedback_btn-turn").on("click", function() {
+	// 	$("#feedback").show();
+	// });
 
 });
